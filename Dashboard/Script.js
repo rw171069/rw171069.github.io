a8a092eee1fd5a9eb17ac20c5aab76dce08e9f5d
rw171069 <33523657+rw171069@users.noreply.github.com>
2018-06-07 21:05:46 +0100

    JS for dashboad

---
+++ Dashboard/Script.js
@@ -0,0 +1,63 @@
+var time = 0;
+var running = 0;
+function startPause(){
+    if(running ==0){
+        running=1;
+        increment();
+        document.getElementById('start').innerHTML = "Pause";
+        document.getElementById('startPause').style.backgroundColor="red";
+        document.getElementById('startPause').style.borderColor="red";
+    } else{
+        running = 0;
+        document.getElementById('start').innerHTML = "Resume";
+        document.getElementById('startPause').style.backgroundColor="green";
+        document.getElementById('startPause').style.borderColor="green";
+    }
+}
+function reset(){
+    running = 0;
+    document.getElementById('start').innerHTML = "Start";
+    document.getElementById('output').innerHTML = "0:00:00:00"
+    document.getElementById('startPause').style.backgroundColor="green";
+    document.getElementById('startPause').style.borderColor="green";
+}
+function increment(){
+    if(running==1){
+        setTimeout(function(){
+            time++;
+            var mins = Math.floor(time/10/60);
+            var secs = Math.floor(time/10%60);
+            var hours = Math.floor(time/10/60/60);
+            var tenth = time%10;
+            if(mins<10){
+                mins ="0" + mins;
+            }
+            if(secs < 10){
+                secs="0"+secs;
+            }
+            document.getElementById("output").innerHTML = hours+":"+mins+":"+secs+":"+tenth+"0";
+            increment();
+        },100)
+    }
+}
+
+const q = x => document.querySelector(x)
+const el = x => document.createElement(x)
+const div = () => el('div')
+const append = (p, c) => p.appendChild(c)
+const timers = q('#timers')
+const button = q('button')
+const hands = ['hours', 'minutes', 'seconds']
+
+const createTimer = () => {
+  const timer = div()
+  hands.forEach(x => {
+    const hand = div()
+    hand.dataset.hand = x
+    append(timer, hand)
+  })
+  append(timers, timer)
+}
+
+button.addEventListener('click', createTimer)
+createTimer()
